9d3f031591cf1473d702c6b6cb6a65d4dea011a6
dillonz <hl520zhangli@163.com>
2019-05-07 19:56:52 +0800

    Initial Commit

---
+++ Exam_0506/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exam_0506
+//
+//  Created by DillonZhang on 2019/5/7.
+//  Copyright © 2019 dzstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exam_0506/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exam_0506
+//
+//  Created by DillonZhang on 2019/5/7.
+//  Copyright © 2019 dzstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
